4576349c3369d48be68ab744b0c04a370303c9ad
ariyanabdullah <ak01859136696@gmail.com>
2022-09-29 07:46:35 +0600

    question file added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
 import Container from './component/Container/Container';
+import Question from './component/Question/Question';
 
 function App() {
   return (
     <div>
      <Header></Header>
      <Container></Container>
+     <Question> </Question>
     </div>
   );
 }
---
+++ src/component/Question/Question.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Question.css'
+
+const Question = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Question;
