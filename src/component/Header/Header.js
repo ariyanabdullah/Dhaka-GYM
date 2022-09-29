733d8dc57cf01e85708da124072e9362eafc2f66
ariyanabdullah <ak01859136696@gmail.com>
2022-09-29 08:42:16 +0600

    fontawosome icon added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css'
 
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
     return (
-        <div className='header-main'>  
-            <h1>Dhaka Gymnasium</h1>
+        <div className='header-main'> 
+        
+            <h1> <span><FontAwesomeIcon icon={faDumbbell} /> </span>  Dhaka Gymnasium</h1>
         </div>
     );
 };
