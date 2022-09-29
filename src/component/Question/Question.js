ea40f70b7b6d9735a8470a0378b9501968b6c0c8
ariyanabdullah <ak01859136696@gmail.com>
2022-09-29 08:04:37 +0600

    third question added

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -12,6 +12,10 @@ const Question = () => {
                 <h1>what is the difference between props and state in react?</h1>
                 <p> = Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
               </div>
+              <div className="first-question">
+                <h1>what does useeffect  hook do in react?</h1>
+                <p> = </p>
+              </div>
         </div>
     );
 };
