8846519061d622c32743ffc22a0b304e06ac75dc
ariyanabdullah <ak01859136696@gmail.com>
2022-09-29 07:53:10 +0600

    first answer added

--- src/component/Question/Question.js
+++ src/component/Question/Question.js
@@ -6,6 +6,7 @@ const Question = () => {
         <div className='all-Question'>
               <div className="first-question">
                 <h1>How does react Work?</h1>
+                <p> = ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering our code</p>
               </div>
         </div>
     );
